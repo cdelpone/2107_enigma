fbb25185dfba7f2977173149da0afeac7e75394c
Christina Delpone <cdelpone@gmail.com>
2021-08-05 15:08:49 -0600

    Add spec_helper

---
+++ spec/spec_helper.rb
@@ -0,0 +1,10 @@
+require 'simplecov'
+
+SimpleCov.start
+
+require 'date'
+require './lib/shift_generator'
+require './lib/enigma'
+require './lib/cipher'
+require './lib/encrypt'
+require './lib/decrypt'
