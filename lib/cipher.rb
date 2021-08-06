a26341b3fba7cb3cfaddfb231a13a0d0f38ac4e0
Christina Delpone <cdelpone@gmail.com>
2021-08-06 16:47:21 -0600

    Refactor find_index and encrypt message arguments/variables

--- lib/cipher.rb
+++ lib/cipher.rb
@@ -3,6 +3,7 @@ class Cipher
 
   def initialize
     @encrypted_message = []
+    @new_message = []
   end
 
   def char_set
@@ -10,29 +11,46 @@ class Cipher
   end
 
   def indexed_char_set
-    char_set.map.with_index(0).to_a
+    @new_char_set = char_set.map.with_index(0).to_a
   end
 
   def split_message(message)
-    message.downcase.split('').flatten
+    # message.downcase.split('').flatten
+    @new_message = (message.downcase.split("")).flatten
   end
 
-  def find_index(index, message)
-  indexed_ltr = indexed_char_set.detect do |index_set|
-      index_set[0] == split_message(message)[index]
+  def find_index(index)
+    @result = char_set.each_index.detect do |i|
+      char_set[i] == @new_message[index]
     end
-    indexed_ltr[1]
+    @result
   end
 
-  def encrypt_message(message)
-    require "pry"; binding.pry
+  # def find_index(index, message)
+  # indexed_ltr = indexed_char_set.detect do |index_set|
+  #     index_set[0] == split_message(message)[index]
+  #   end
+  #   indexed_ltr[1]
+  # end
+
+  def encrypt_message
     shift = ShiftGenerator.new
-    split_message(message).each do |letter|
-      find_index(split_message(message).index(letter))
-      new_char_index = find_index + shift.all_shifts.rotate!(3)[0]
+    @new_message.each do |letter|
+      find_index(@new_message.index(letter))
+      new_char_index = @result + shift.all_shifts.rotate!(3)[0]
       @encrypted_message << char_set[new_char_index.to_i % 27]
     end
     @encrypted_message.join
   end
-
 end
+  # def encrypt_message(message)
+  #   require "pry"; binding.pry
+  #   shift = ShiftGenerator.new
+  #   split_message(message).each do |letter|
+  #     find_index(split_message(message).index(letter))
+  #     new_char_index = find_index + shift.all_shifts.rotate!(3)[0]
+  #     @encrypted_message << char_set[new_char_index.to_i % 27]
+  #   end
+  #   @encrypted_message.join
+  # end
+# end
