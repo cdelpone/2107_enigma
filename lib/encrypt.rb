9d42ae0afa58ebf6dee0c938ebf4a3dd731d44bd
Christina Delpone <cdelpone@gmail.com>
2021-08-07 17:08:14 -0600

    Write encrypted message to file

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -2,13 +2,13 @@ require_relative 'enigma'
 
 class Encrypt
 
-  @enigma_machine = Enigma.new
+  @enigma = Enigma.new
 
   file = File.open(ARGV.first, "r")
 
   text = file.read.chomp
 
-  result = @enigma_machine.encrypt(text)
+  result = @enigma.encrypt(text)
 
   file_output = ARGV.last
 
@@ -16,5 +16,5 @@ class Encrypt
 
   new_file.write(result[:encryption])
 
-  puts "Created #{file} with the key 82648 and date 240818"
+  puts "Created #{file} with the key #{result[:key]} and date #{result[:date]}"
 end
