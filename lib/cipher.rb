8863d1542a93ed0830b7e4430b80a4a72352d260
Christina Delpone <cdelpone@gmail.com>
2021-08-08 17:05:42 -0600

    Remove unused methods, refactor to pass tests

--- lib/cipher.rb
+++ lib/cipher.rb
@@ -1,49 +1,50 @@
 class Cipher
   attr_reader :encrypted_message,
-              :decrypted_message
+              :decrypted_message,
+              :message
 
-  def initialize
+  def initialize(message)
+    @message = message
     @encrypted_message = []
     @decrypted_message = []
-    @message = []
   end
 
   def char_set
     ("a".."z").to_a << " "
   end
 
-  def indexed_char_set
-    @new_char_set = char_set.map.with_index(0).to_a
-  end
-
-  def split_message(message)
-    @message = (message.downcase.split("")).flatten
+  def change_message
+    message.downcase.split("")
   end
 
   def find_index(index)
     @result = char_set.each_index.detect do |i|
-      char_set[i] == @message[index]
+      char_set[i] == change_message[index]
     end
-    @result
   end
 
-  def encrypt_message(message)
-    shift = ShiftGenerator.new
-    @message.each do |letter|
-      find_index(@message.index(letter))
-      new_char_index = @result + shift.all_shifts.rotate!(3)[0]
+  def encrypt_message
+    shift = ShiftGenerator.new('02715', '040895')
+    current_shift = shift.all_shifts
+    change_message.each do |letter|
+      find_index(change_message.index(letter))
+      new_char_index = @result.to_i + (current_shift[0]).to_i
       @encrypted_message << char_set[new_char_index.to_i % 27]
+      current_shift = current_shift.rotate
     end
     @encrypted_message.join
   end
 
-  def decrypt_message(message)
-    shift = ShiftGenerator.new
-    @message.each do |letter|
-      find_index(@message.index(letter))
-      new_char_index = @result - shift.all_shifts.rotate!(3)[0]
+  def decrypt_message
+    shift = ShiftGenerator.new('02715', '040895')
+    current_shift = shift.all_shifts
+    change_message.each do |letter|
+      find_index(change_message.index(letter))
+      new_char_index = @result.to_i - (current_shift[0]).to_i
       @decrypted_message << char_set[new_char_index.to_i % 27]
+      current_shift = current_shift.rotate
     end
     @decrypted_message.join
+    # require "pry"; binding.pry
   end
 end
--- spec/cipher_spec.rb
+++ spec/cipher_spec.rb
@@ -2,43 +2,32 @@ require 'spec_helper'
 
 RSpec.describe Cipher do
   before :each do
-    @cipher = Cipher.new
-    @shift = ShiftGenerator.new
+    @cipher = Cipher.new('Hello world!')
+    @cipher2 = Cipher.new('keder ohulw')
   end
 
   it 'exists' do
     expect(@cipher).to be_an Cipher
+    expect(@cipher.message).to eq('Hello world!')
   end
 
   it 'returns 27 character set' do
     expect(@cipher.char_set).to eq(['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', ' '])
     end
 
-  it 'indexes the char set' do
-    expect(@cipher.indexed_char_set).to eq([['a', 0], ['b', 1], ['c', 2], ['d', 3], ['e', 4], ['f', 5], ['g', 6], ['h', 7], ['i', 8], ['j', 9], ['k', 10], ['l', 11], ['m', 12], ['n', 13], ['o', 14], ['p', 15], ['q', 16], ['r', 17], ['s', 18], ['t', 19], ['u', 20], ['v', 21], ['w', 22], ['x', 23], ['y', 24], ['z', 25], [' ', 26]])
-  end
-
   it 'turns the incoming message to an array of all lower case' do
-    expect(@cipher.split_message('hello world')).to eq(['h', 'e', 'l', 'l', 'o', ' ', 'w', 'o', 'r', 'l', 'd'])
+    expect(@cipher.change_message).to eq(['h', 'e', 'l', 'l', 'o', ' ', 'w', 'o', 'r', 'l', 'd', '!'])
   end
 
   it 'returns char set index number for given letter' do
-    message = 'hello world'
-    @cipher.split_message(message)
-    split_message = ["h", "e", "l", "l", "o", " ", "w", "o", "r", "l", "d"]
-
     expect(@cipher.find_index(0)).to eq(7)
   end
 
   it 'returns the encrypted message' do
-    @message = 'hello world'
-    @cipher.split_message(@message)
-    expect(@cipher.encrypt_message('hello world')).to eq('keder ohulw')
+    expect(@cipher.encrypt_message).to eq('keder ohulw')
   end
 
   it 'decrypts the encrypted message' do
-    @message = "keder ohulw"
-    @cipher.split_message(@message)
-    expect(@cipher.decrypt_message('keder ohulw')).to eq('hello world')
+    expect(@cipher2.decrypt_message).to eq('hello world')
   end
 end
