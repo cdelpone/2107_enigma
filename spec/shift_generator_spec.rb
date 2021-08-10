c00f299261d73eed55ece36daa3666faee7084b3
Christina Delpone <cdelpone@gmail.com>
2021-08-09 19:51:06 -0600

    Add test for transmission date to return todays date

--- spec/shift_generator_spec.rb
+++ spec/shift_generator_spec.rb
@@ -3,6 +3,8 @@ require 'spec_helper'
 RSpec.describe ShiftGenerator do
   before :each do
     @shift = ShiftGenerator.new('02715', '040895')
+    @shift2 = ShiftGenerator.new('02715', date = nil)
+
   end
 
   it 'exists' do
@@ -28,6 +30,7 @@ RSpec.describe ShiftGenerator do
 
   it 'returns todays date as a string' do
     expect(@shift.transmission_date).to eq('040895')
+    expect(@shift2.transmission_date).to eq("090821")
   end
 
   it 'returns and assigns offsets' do
